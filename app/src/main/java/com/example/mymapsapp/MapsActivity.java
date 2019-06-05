f0b4a0b44086a5d3725fe54003640d976153fbdd
Chloe Chan <icefiresparrow@gmail.com>
2019-06-05 11:48:56 -0700

    Updated Maps Activity to find birth place marker, current location, and switch map view type

--- app/src/main/java/com/example/mymapsapp/MapsActivity.java
+++ app/src/main/java/com/example/mymapsapp/MapsActivity.java
@@ -1,7 +1,16 @@
 package com.example.mymapsapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +22,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private boolean isGPSEnabled;
+    private boolean isNetworkEnabled;
+
+    private static final long MIN_TIME_BW_UPDATES = 1000*5;
+    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 0.0f;
+    //private locationListenerNetwork;
+    //private locationListenerGPS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +39,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+    }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -39,8 +55,83 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng calgary = new LatLng(51, -114);
+        mMap.addMarker(new MarkerOptions().position(calgary).title("Chloe's Birth Place"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(calgary));
+
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            Log.d("MyMapsApp","Failed Permission Check 1");
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            Log.d("MyMapsApp","Failed Permission Check 2");
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 200);
+        }
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Log.d("MyMapsApp","Dropping marker at my location");
+            mMap.setMyLocationEnabled(true);
+        }
+
     }
+
+/*
+    public void getLocation() {
+
+
+        try{
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            //Get GPS Status
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if(isGPSEnabled) Log.d("MyMapsApp","getLocation: GPS is enabled");
+
+            //Get network status (cell tower + wifi)
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if(isNetworkEnabled) Log.d("MyMapsApp","getLocation: Network is enabled");
+
+
+            if(!isGPSEnabled && !isNetworkEnabled) //no provider is enabled
+                Log.d("MyMapsApp","getLocation: no provider is enabled");
+            else {
+                if(isNetworkEnabled){
+                    Log.d("MyMapsApp","getLocation: network provider is enabled");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                            locationListenerNetwork);
+                }
+                if(isGPSEnabled){
+                    Log.d("MyMapsApp","getLocation: GPS provider is enabled");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                            locationListenerGPS);
+                }
+            }
+        }
+        catch (Exception e){
+            Log.d("MyMapsApp","getLocation: Printing stack trace");
+            e.printStackTrace();
+        }
+    }
+*/
+
+
+    public void changeView(View view)  {
+
+        //change between satellite and map view
+        if(mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            Log.d("MyMapsApp","changeView: Changing to satellite view");
+
+        }
+        else{
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            Log.d("MyMapsApp","changeView: Changing to map view");
+
+        }
+    }
+
 }
